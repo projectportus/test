acb1ec6ad891fcd6d1833f01ed6f2711cf91e5a2
Iskander <iskander102005@gmail.com>
2025-07-03 12:47:09 -0400

    first push in main

--- Testich/JavaGit.java
+++ Testich/JavaGit.java
@@ -3,5 +3,7 @@ package Testich;
 public class JavaGit {
     public static void main(String[] args) {
         System.out.println("Main Test");
+
+        System.out.println("First Change in Main");
     }
 }
